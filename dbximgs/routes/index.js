17cca0bf6d7d3ea739a4c6f21b0408baee0b9089
Craig Gilbertson <44072634+crvgilbertson@users.noreply.github.com>
2018-11-29 04:59:50 +0000

    Add files via upload

---
+++ dbximgs/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+// the reference to the controller
+var controller = require('../controller');
+
+/* GET home page. */
+router.get('/', controller.home);
+router.get('/login', controller.login);
+router.get('/oauthredirect',controller.oauthredirect);
+
+module.exports = router;
